7074f7e805542bea5b62409359a02480a12135a8
tkofsq <bcharlesem@gmail.com>
2026-03-01 14:32:37 +0800

    changed theme.css

---
+++ src/shared/scripts/supabase.js
@@ -0,0 +1,7 @@
+
+import { createClient } from 'https://esm.sh/@supabase/supabase-js'
+
+window.supabase = createClient(
+        'https://moeyonokrqnsrhthkjyd.supabase.co',
+        'sb_publishable_xdY5f0sBT-bzAqOBmCV0Eg_B6Vk3Z_C'
+)
