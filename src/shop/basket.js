03514a9af39d86dd361dcf6c9508fa05a5c786d6
tkofsq <bcharlesem@gmail.com>
2026-03-01 23:59:18 +0800

    js distribution, *missing filter.js or filter functions in catalog.js*

--- src/shop/basket.js
+++
@@ -1,7 +0,0 @@
-const addToBasket = (_) => {
-        let basket = JSON.parse(localStorage.getItem("basket")) || []
-        
-        basket.push({ id: _ })
-        
-        localStorage.setItem("basket", JSON.stringify(basket))
-}
--- src/shop/catalog.js
+++ src/shop/catalog.js
@@ -39,3 +39,11 @@ window.addEventListener('DOMContentLoaded', async () => {
                 })
         }
 })
+
+const addToBasket = (_) => {
+        let basket = JSON.parse(localStorage.getItem("basket")) || []
+        
+        basket.push({ id: _ })
+        
+        localStorage.setItem("basket", JSON.stringify(basket))
+}
